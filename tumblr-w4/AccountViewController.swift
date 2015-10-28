d3b4dad35a6268a24f53449b70b14a230d0aa1ea
Ciro Verdi <ciroverdi@gmail.com>
2015-10-28 10:36:27 -0200

    Creating AccountViewController view

---
+++ tumblr-w4/AccountViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AccountViewController.swift
+//  tumblr-w4
+//
+//  Created by cverdi on 10/28/15.
+//  Copyright © 2015 walmart. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
