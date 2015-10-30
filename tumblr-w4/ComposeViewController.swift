dcc738857dd607ff8d384899a26776d0bca20d68
Ciro Verdi <ciroverdi@gmail.com>
2015-10-30 20:03:01 -0200

    Create Compose Buttons Transitions

--- tumblr-w4/ComposeViewController.swift
+++ tumblr-w4/ComposeViewController.swift
@@ -11,14 +11,66 @@ import UIKit
 class ComposeViewController: UIViewController {
 
     @IBOutlet var mainView: UIView!
+    @IBOutlet weak var textImageView: UIImageView!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var quoteImageView: UIImageView!
+    @IBOutlet weak var linkImageView: UIImageView!
+    @IBOutlet weak var chatImageView: UIImageView!
+    @IBOutlet weak var videoImageView: UIImageView!
+    
+    var fadeTransition: FadeTransition!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView.backgroundColor = UIColorFromRGB(0x33465D)
-        mainView.alpha = 0.5
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        textImageView.frame = CGRect(x: 20, y: 174, width: 80, height: 90)
+        photoImageView.frame = CGRect(x: 120, y: 174, width: 80, height: 90)
+        quoteImageView.frame = CGRect(x: 220, y: 174, width: 80, height: 90)
+        linkImageView.frame = CGRect(x: 20, y: 294, width: 80, height: 90)
+        chatImageView.frame = CGRect(x: 120, y: 294, width: 80, height: 90)
+        videoImageView.frame = CGRect(x: 220, y: 294, width: 80, height: 90)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        textImageView.frame = CGRect(x: 20, y: 174, width: 80, height: 90)
+        photoImageView.frame = CGRect(x: 120, y: 174, width: 80, height: 90)
+        quoteImageView.frame = CGRect(x: 220, y: 174, width: 80, height: 90)
+        linkImageView.frame = CGRect(x: 20, y: 294, width: 80, height: 90)
+        chatImageView.frame = CGRect(x: 120, y: 294, width: 80, height: 90)
+        videoImageView.frame = CGRect(x: 220, y: 294, width: 80, height: 90)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func goBack(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        // Access the ViewController that you will be transitioning too, a.k.a, the destinationViewController.
+        var destinationViewController = segue.destinationViewController
+        
+        // Set the modal presentation style of your destinationViewController to be custom.
+        destinationViewController.modalPresentationStyle = UIModalPresentationStyle.Custom
+        
+        // Create a new instance of your fadeTransition.
+        fadeTransition = FadeTransition()
+        
+        // Tell the destinationViewController's  transitioning delegate to look in fadeTransition for transition instructions.
+        destinationViewController.transitioningDelegate = fadeTransition
+        
+        // Adjust the transition duration. (seconds)
+        fadeTransition.duration = 1.0
     }
 }
--- tumblr-w4/TabBarViewController.swift
+++ tumblr-w4/TabBarViewController.swift
@@ -11,15 +11,61 @@ import UIKit
 class TabBarViewController: UIViewController {
 
     @IBOutlet weak var tabView: UIView!
+    @IBOutlet weak var tabBarView: UIView!
     @IBOutlet weak var contentView: UIView!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    var viewControllers: [UIViewController]!
+    var homeViewController: UIViewController!
+    var searchViewController: UIViewController!
+    var accountViewController: UIViewController!
+    var trendingViewController: UIViewController!
+    
+    var tabIndex: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tabView.backgroundColor = UIColorFromRGB(0x33465D)
+        tabBarView.backgroundColor = UIColorFromRGB(0x334256)
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        homeViewController = storyboard.instantiateViewControllerWithIdentifier("HomeViewController")
+        searchViewController = storyboard.instantiateViewControllerWithIdentifier("SearchViewController")
+        accountViewController = storyboard.instantiateViewControllerWithIdentifier("AccountViewController")
+        trendingViewController = storyboard.instantiateViewControllerWithIdentifier("TrendingViewController")
+        
+        viewControllers = [homeViewController, searchViewController, accountViewController, trendingViewController]
+        
+        buttons[tabIndex].selected = true
+        didPressTab(buttons[tabIndex])
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    @IBAction func didPressTab(sender: UIButton) {
+        let previousIndex = tabIndex
+        
+        tabIndex = sender.tag
+
+        buttons[previousIndex].selected = false
+        
+        let previousVC = viewControllers[previousIndex]
+        
+        previousVC.willMoveToParentViewController(nil)
+        previousVC.view.removeFromSuperview()
+        previousVC.removeFromParentViewController()
+        
+        sender.selected = true
+        
+        let vc = viewControllers[tabIndex]
+        addChildViewController(vc)
+        
+        vc.view.frame = contentView.bounds
+        contentView.addSubview(vc.view)
+        
+        vc.didMoveToParentViewController(self)
+    }
 }
